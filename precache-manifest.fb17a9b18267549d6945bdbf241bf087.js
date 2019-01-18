0a1f852c7092bcd3ca1bc3a1fa92cbe1bbb5bb05
dtsuper3 <dtsuper3@gmail.com>
2019-01-18 23:52:34 +0530

    Updates

---
+++ precache-manifest.fb17a9b18267549d6945bdbf241bf087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "b07ede133324d33c9d44",
+    "url": "/myportfolio/static/js/main.b07ede13.chunk.js"
+  },
+  {
+    "revision": "0ec98559c3245a4c8f06",
+    "url": "/myportfolio/static/js/1.0ec98559.chunk.js"
+  },
+  {
+    "revision": "b07ede133324d33c9d44",
+    "url": "/myportfolio/static/css/main.0e5dc04d.chunk.css"
+  },
+  {
+    "revision": "0ec98559c3245a4c8f06",
+    "url": "/myportfolio/static/css/1.46003cd3.chunk.css"
+  },
+  {
+    "revision": "fc89dd7da6d67f17cfb5ebdad8837302",
+    "url": "/myportfolio/index.html"
+  }
+];
