3165a937a5ff96b14a74c35d56b43482ea80ce0f
dtsuper3 <dtsuper3@gmail.com>
2019-09-04 12:50:12 +0530

    favicon added, new projects added

--- src/components/projects.js
+++ src/components/projects.js
@@ -32,18 +32,38 @@ class Projects extends Component {
               style={{
                 color: "#fff",
                 height: "176px",
-                background:"#333"             
+                background: "#333"
               }}
             >
               {/*bottom right 15% no-repeat #46B6AC*/}
               GitHub Finder
             </CardTitle>
             <CardText>
-            The app uses github api to fetch github user public profile data. The app shows various user detail like latest five repos,total repository etc
+              The app uses github api to fetch github user public profile data.
+              The app shows various user detail like latest five repos,total
+              repository etc
             </CardText>
             <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/frontend-projects/tree/master/Github%20Finder" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">GitHub</a></Button>              
-              <Button colored><a href="https://dtsuper3.github.io/frontend-projects/Github%20Finder/" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">LiveDemo</a></Button>              
+              <Button colored>
+                <a
+                  href="https://github.com/dtsuper3/frontend-projects/tree/master/Github%20Finder"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </Button>
+              <Button colored>
+                <a
+                  href="https://dtsuper3.github.io/frontend-projects/Github%20Finder/"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LiveDemo
+                </a>
+              </Button>
             </CardActions>
             <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
@@ -60,18 +80,38 @@ class Projects extends Component {
               style={{
                 color: "#fff",
                 height: "176px",
-                background:"#333"             
+                background: "#333"
               }}
             >
               {/*bottom right 15% no-repeat #46B6AC*/}
               Tracalorie
             </CardTitle>
             <CardText>
-            This is an web app which is used for tracking calorie of various meal.The app uses LocalStorage to persistant the entered data.We can update, delete and clear all the entered data  
+              This is an web app which is used for tracking calorie of various
+              meal.The app uses LocalStorage to persistant the entered data.We
+              can update, delete and clear all the entered data
             </CardText>
             <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/frontend-projects/tree/master/Tracalorie" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>GitHub</a></Button>              
-              <Button colored><a href="https://dtsuper3.github.io/frontend-projects/Tracalorie/" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>LiveDemo</a></Button>              
+              <Button colored>
+                <a
+                  href="https://github.com/dtsuper3/frontend-projects/tree/master/Tracalorie"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ textDecoration: "none" }}
+                >
+                  GitHub
+                </a>
+              </Button>
+              <Button colored>
+                <a
+                  href="https://dtsuper3.github.io/frontend-projects/Tracalorie/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ textDecoration: "none" }}
+                >
+                  LiveDemo
+                </a>
+              </Button>
             </CardActions>
             <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
@@ -88,102 +128,37 @@ class Projects extends Component {
               style={{
                 color: "#fff",
                 height: "176px",
-                background:"#333"             
-              }}
-            >
-              {/*bottom right 15% no-repeat #46B6AC*/}
-              Weather App
-            </CardTitle>
-            <CardText>
-            This web app is used to show the weather information.
-            </CardText>
-            <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/frontend-projects/tree/master/Weather%20App" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>GitHub</a></Button>              
-              <Button colored><a href="https://dtsuper3.github.io/frontend-projects/Weather%20App/" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>LiveDemo</a></Button>              
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-          {/* Project 4*/}
-          <Card
-            shadow={5}
-            style={{ minWidth: "450", margin: "auto" }}
-            className="card"
-          >
-            <CardTitle
-              expand
-              style={{
-                color: "#fff",
-                height: "176px",
-                background:"#333"             
+                background: "#333"
               }}
             >
               {/*bottom right 15% no-repeat #46B6AC*/}
               Loan Calculator
             </CardTitle>
             <CardText>
-              This web app is used to calculate total loan amount and loan interest.
+              This web app is used to calculate total loan amount and loan
+              interest.
             </CardText>
             <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/frontend-projects/tree/master/Loan%20Calculator" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>GitHub</a></Button>              
-              <Button colored><a href="https://dtsuper3.github.io/frontend-projects/Loan%20Calculator/" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>LiveDemo</a></Button>              
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-          {/* Project 5*/}
-          <Card
-            shadow={5}
-            style={{ minWidth: "450", margin: "auto" }}
-            className="card"
-          >
-            <CardTitle
-              expand
-              style={{
-                color: "#fff",
-                height: "176px",
-                background:"#333"             
-              }}
-            >
-              {/*bottom right 15% no-repeat #46B6AC*/}
-              TaskList
-            </CardTitle>
-            <CardText>
-              TaskList
-            </CardText>
-            <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/frontend-projects/tree/master/TaskList" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>GitHub</a></Button>              
-              <Button colored><a href="https://dtsuper3.github.io/frontend-projects/TaskList/" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>LiveDemo</a></Button>              
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-          {/* Project 6*/}
-          <Card
-            shadow={5}
-            style={{ minWidth: "450", margin: "auto" }}
-            className="card"
-          >
-            <CardTitle
-              expand
-              style={{
-                color: "#fff",
-                height: "176px",
-                background:"#333"             
-              }}
-            >
-              {/*bottom right 15% no-repeat #46B6AC*/}
-              Particle Form
-            </CardTitle>
-            <CardText>
-            Particle Form
-            </CardText>
-            <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/particle_form" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>GitHub</a></Button>              
-              <Button colored><a href="https://dtsuper3.github.io/particle_form/" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none'}}>LiveDemo</a></Button>              
+              <Button colored>
+                <a
+                  href="https://github.com/dtsuper3/frontend-projects/tree/master/Loan%20Calculator"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ textDecoration: "none" }}
+                >
+                  GitHub
+                </a>
+              </Button>
+              <Button colored>
+                <a
+                  href="https://dtsuper3.github.io/frontend-projects/Loan%20Calculator/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ textDecoration: "none" }}
+                >
+                  LiveDemo
+                </a>
+              </Button>
             </CardActions>
             <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
@@ -205,18 +180,80 @@ class Projects extends Component {
               style={{
                 color: "#fff",
                 height: "176px",
-                background:"#333"             
+                background: "#333",
+                minHeight: "200"
               }}
             >
               {/*bottom right 15% no-repeat #46B6AC*/}
               React Store Cart
             </CardTitle>
-            <CardText>
-              This is a simple shopping cart.The UI is created by using React.
-            </CardText>
+            <CardText>This is a simple shopping cart.</CardText>
             <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/react-store-cart" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">GitHub</a></Button>              
-              <Button colored><a href="https://react-store-cart.netlify.com/" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">LiveDemo</a></Button>              
+              <Button colored>
+                <a
+                  href="https://github.com/dtsuper3/react-store-cart"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </Button>
+              <Button colored>
+                <a
+                  href="https://react-store-cart.netlify.com/"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LiveDemo
+                </a>
+              </Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          {/*Project #2*/}
+          <Card
+            shadow={5}
+            style={{ minWidth: "450", margin: "auto" }}
+            className="card"
+          >
+            <CardTitle
+              expand
+              style={{
+                color: "#fff",
+                height: "176px",
+                background: "#333",
+                minHeight: "200"
+              }}
+            >
+              {/*bottom right 15% no-repeat #46B6AC*/}
+              DTDevConnector
+            </CardTitle>
+            <CardText>This is a MERN stack app.</CardText>
+            <CardActions border>
+              {/* <Button colored>
+                <a
+                  href="https://github.com/dtsuper3/react-store-cart"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </Button> */}
+              <Button colored>
+                <a
+                  href="https://dtdevconnector.herokuapp.com"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LiveDemo
+                </a>
+              </Button>
             </CardActions>
             <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
@@ -238,23 +275,29 @@ class Projects extends Component {
               style={{
                 color: "#fff",
                 height: "176px",
-                background:"#333"             
+                background: "#333"
               }}
             >
               {/*bottom right 15% no-repeat #46B6AC*/}
-              Node Chat App
+              URL Shortner
             </CardTitle>
-            <CardText>
-              A simple web socket application.
-            </CardText>
+            <CardText>A microservice in nodeJs for url shortning.</CardText>
             <CardActions border>
-              <Button colored><a href="https://github.com/dtsuper3/web-socket-chat" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">GitHub</a></Button>              
-              <Button colored><a href="https://tranquil-garden-83622.herokuapp.com/" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">LiveDemo</a></Button>              
+              <Button colored>
+                <a
+                  href="https://api-project-url-shortener-microservice.glitch.me/"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LiveDemo
+                </a>
+              </Button>
             </CardActions>
             <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
             </CardMenu>
-          </Card>                    
+          </Card>
           {/*Project #2*/}
           <Card
             shadow={5}
@@ -266,29 +309,29 @@ class Projects extends Component {
               style={{
                 color: "#fff",
                 height: "176px",
-                background:"#333"             
+                background: "#333"
               }}
             >
               {/*bottom right 15% no-repeat #46B6AC*/}
-              URL Shortner
+              Weather App
             </CardTitle>
-            <CardText>
-              A microservice in nodeJs for url shortning.
-            </CardText>
+            <CardText>A simple weather app.</CardText>
             <CardActions border>
-              {/* <Button colored><a href="https://github.com/dtsuper3/web-socket-chat" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">GitHub</a></Button>               */}
-              <Button colored><a href="https://api-project-url-shortener-microservice.glitch.me/" style={{textDecoration:'none'}} target="_blank" rel="noopener noreferrer">LiveDemo</a></Button>              
+              <Button colored>
+                <a
+                  href="https://dtsuper3-weather-app.herokuapp.com/"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LiveDemo
+                </a>
+              </Button>
             </CardActions>
             <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>This is PHP.</h1>
         </div>
       );
     }
@@ -302,10 +345,9 @@ class Projects extends Component {
           onChange={tabId => this.setState({ activeTab: tabId })}
           ripple
         >
-          <Tab>Frontend Projects</Tab>
+          <Tab>Vanilla JS</Tab>
           <Tab>React</Tab>
           <Tab>NodeJs</Tab>
-          {/* <Tab>PHP</Tab> */}
         </Tabs>
         <section className="category-grid">
           <Grid>
